9b5abf74836696198ec4eb04824c02bac9dda538
jbride <jbride2001@yahoo.com>
2021-01-19 19:03:23 -0700

    migration_journal:  24 Jan 2020 notes

--- src/test/java/com/redhat/cajun/navy/process/MissionLifecycleTest.java
+++ src/test/java/com/redhat/cajun/navy/process/MissionLifecycleTest.java
@@ -1,5 +1,6 @@
 package com.redhat.cajun.navy.process;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.time.OffsetDateTime;
@@ -13,6 +14,7 @@ import javax.inject.Inject;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.After;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.kie.kogito.kafka.KafkaClient;
 import org.kie.kogito.testcontainers.quarkus.KafkaQuarkusTestResource;
@@ -20,22 +22,31 @@ import org.jboss.logging.Logger;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.cloudevents.CloudEvent;
 import io.cloudevents.core.builder.CloudEventBuilder;
+import io.cloudevents.jackson.JsonFormat;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+
+import org.kie.kogito.services.event.AbstractProcessDataEvent;
 
 import com.redhat.cajun.navy.rules.model.Mission;
 import com.redhat.cajun.navy.rules.model.Incident;
 import com.redhat.cajun.navy.rules.model.Status;
 
+/*
+    Inspired by:  https://github.com/kiegroup/kogito-examples/blob/stable/process-kafka-quickstart-quarkus/src/test/java/org/acme/travel/MessagingIT.java
+*/
 @QuarkusTest
 @QuarkusTestResource(KafkaQuarkusTestResource.class)
 public class MissionLifecycleTest {
 
-    private static final String  TOPIC_MISSION_EVENT = "topic-mission-event";
+    private static final String TOPIC_MISSION_EVENT = "topic-mission-event";
     private static final String TOPIC_INCIDENT_COMMAND = "topic-incident-command";
+    private static final String DATA = "data";
 
     private static Logger log = Logger.getLogger(MissionLifecycleTest.class);
     
@@ -49,28 +60,77 @@ public class MissionLifecycleTest {
 
     private long sleepBetweenStateChanges=30000;
 
+    @BeforeEach
+    public void setup() {
+	/*
+            The following jackson configs are critical to ensure CloudEvents are (un)marshalled appropriately
+            Otherwise, expect to encounter exceptions such as the following
+
+		com.fasterxml.jackson.databind.exc.MismatchedInputException: Cannot construct instance of `com.redhat.cajun.navy.rules.model.Mission` (although at least one Creator exists): no String-argument constructor/factory method to deserialize from String value ('eyJpbmNpZGVudElkIjoiaW5jaWRlbnQxIiwicmVzcG9uZGVySWQiOiJyZXNwb25kZXIxIiwic3RhdHVzIjoiVU5BU1NJR05FRCIsInJlc3BvbmRlclN0YXJ0TGF0IjoyLCJyZXNwb25kZXJTdGFydExvbmciOjIsImluY2lkZW50TGF0IjowLCJpbmNpZGVudExvbmciOjAsImRlc3RpbmF0aW9uTGF0IjoxLCJkZXN0aW5hdGlvbkxvbmciOjEsImxhc3RVcGRhdGUiOjE2MTExNzU2NjI3Njd9')
+         */
+        objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        objectMapper.registerModule(JsonFormat.getCloudEventJacksonModule());
+    }
+
+    //@Test
+    public void testMissionSerialization() throws IOException {
+        Mission missionObj = createMission();
+        byte[] missionStringBytes = objectMapper.writeValueAsString(missionObj).getBytes();
+
+        Mission unmarshalledMission = objectMapper.readValue(missionStringBytes, Mission.class);
+        log.info("testMissionSerialization() unmarshalledMission = "+unmarshalledMission); 
+    }
+
+    //@Test
+    public void testMissionAsBytesSerialization() throws IOException {
+        Mission missionObj = createMission();
+        byte[] missionJsonBytes = objectMapper.writeValueAsString(missionObj).getBytes();  // Get bytes of JSON representation of Mission
+        MissionWrapper mWrapper = new MissionWrapper();
+        mWrapper.setMissionBytes(missionJsonBytes);
+        String mWrapperJson = objectMapper.writeValueAsString(mWrapper);  // Get JSON representation of MissionWrapper
+
+        MissionWrapper unMarshalledMWrapper = objectMapper.readValue(mWrapperJson, MissionWrapper.class);
+        log.info("testMissionAsByteSerialization() missionWrapperJsonAsBytes = "+unMarshalledMWrapper.getMissionBytes());
+
+        Mission unMarshalledMission = objectMapper.readValue(unMarshalledMWrapper.getMissionBytes(), Mission.class); // Using byte[] representation of JSON, unmarshall to Mission 
+        log.info("testMissionAsByteSerialization() unmarshalledMission = "+unMarshalledMission);
+
+    }
+
+    //@Test
+    public void testCloudEventMarshalling() throws IOException {
+        try {
+            Mission missionObj = createMission();
+            String cloudEventJson = generateCloudEventJson(missionObj);
+            log.info("testCloudEventSerialization() mEventJson = "+cloudEventJson); 
+    
+            MissionLifecycleMessageDataEvent cloudEventObj = objectMapper.readValue(cloudEventJson, MissionLifecycleMessageDataEvent.class);
+            
+            log.info("testCloudEventSerialization() mission = "+cloudEventObj.getData()); 
+        }catch(JsonProcessingException x) {
+            x.printStackTrace();
+        }
+    }
+
     @Test
     public void testProcess() throws InterruptedException, JsonProcessingException {
         objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
         kafkaClient = new KafkaClient(kafkaBootstrapServers);
         Mission missionObj = createMission();
 
-        // https://github.com/kiegroup/kogito-examples/blob/stable/process-kafka-quickstart-quarkus/src/test/java/org/acme/travel/MessagingIT.java#L74-L96
-        // https://github.com/kiegroup/kogito-apps/blob/master/data-index/data-index-service/data-index-service-common/src/test/java/org/kie/kogito/index/messaging/AbstractReactiveMessagingEventConsumerKafkaIT.java
-        // https://smallrye.io/smallrye-reactive-messaging/smallrye-reactive-messaging/2.4/model/model.html#skipping
-
         // Send initial mission event to business process and consume Incident with status of:  Assigned
-        kafkaClient.consume(TOPIC_INCIDENT_COMMAND, iString -> {
+        kafkaClient.consume(TOPIC_INCIDENT_COMMAND, iJson -> {
             try {
-                Incident iObj = objectMapper.readValue(iString, Incident.class);
+                JsonNode event = objectMapper.readValue(iJson, JsonNode.class);
+                Incident iObj = objectMapper.readValue(event.get(DATA).toString(), Incident.class);
                 log.infov("Received incident with status: {0}", iObj.getStatus());
                 assertEquals(iObj.getStatus(), Status.ASSIGNED.name());
-            } catch (JsonProcessingException e) {
-                log.error("Error parsing {}", iString, e);
+            } catch (Throwable e) {
+                log.error("Error parsing {}", iJson, e);
                 throw new RuntimeException(e);
             }
         });
-        sendMissionEvent(missionObj, Status.UNASSIGNED, TOPIC_MISSION_EVENT);
+        sendCloudEvent(missionObj, Status.UNASSIGNED, TOPIC_MISSION_EVENT);
 
 /*
         kafkaClient.consume(I_MISSION_STARTED_TOPIC_CHANNEL, s -> {
@@ -110,14 +170,7 @@ public class MissionLifecycleTest {
         sendEvent(missionObj, Mission.Status.DROPPED, O_INCIDENT_COMMAND_DELIVERED);
 */
     }
-    
-    private void sendMissionEvent(Mission missionObj, Status mStatus, String topic) throws JsonProcessingException, InterruptedException {
-        missionObj.setStatus(mStatus);
-        String mJson = generateMissionEvent(missionObj);
-        kafkaClient.produce(mJson, topic);
-        log.infov("Sent event to topic: {0}", topic);
-        Thread.sleep(sleepBetweenStateChanges);
-    }
+
 
     private Mission createMission() {
         Mission mObj = new Mission();
@@ -133,19 +186,58 @@ public class MissionLifecycleTest {
         return mObj;
     }
 
-    private String generateMissionEvent(Mission mObj) throws JsonProcessingException {
+    private String generateCloudEventJson(Mission mObj) throws JsonProcessingException {
+
+        String jsonMission = objectMapper.writeValueAsString(mObj);
+
+        CloudEvent cloudEvent = CloudEventBuilder.v1()
+            .withId(UUID.randomUUID().toString())
+            .withSource(URI.create(""))
+            .withType("MissionLifecycleMessageDataEvent_1") // example: TravelersMessageDataEvent_3
+            .withTime(OffsetDateTime.now())
+            .withData(jsonMission.getBytes())
+            .build();
+        log.info("generateMissionEvent() cloudEvent = "+cloudEvent.getClass().toString());
       
-      return objectMapper.writeValueAsString(CloudEventBuilder.v1()
-                    .withId(UUID.randomUUID().toString())
-                    .withSource(URI.create(""))
-                    .withType("Mission")
-                    .withTime(OffsetDateTime.now())
-                    .withData(objectMapper.writeValueAsString(mObj).getBytes())
-                    .build());
+        return objectMapper.writeValueAsString(cloudEvent);
+    }
+    
+    private void sendCloudEvent(Mission missionObj, Status mStatus, String topic) throws JsonProcessingException, InterruptedException {
+        missionObj.setStatus(mStatus);
+        String mJson = generateCloudEventJson(missionObj);
+        kafkaClient.produce(mJson, topic);
+        log.infov("Sent event to topic: {0}", topic);
+        Thread.sleep(sleepBetweenStateChanges);
     }
 
     @After
     public void stop() {
         Optional.ofNullable(kafkaClient).ifPresent(KafkaClient::shutdown);
+    }
+}
+
+class MissionLifecycleMessageDataEvent extends AbstractProcessDataEvent<com.redhat.cajun.navy.rules.model.Mission> {
+
+    public MissionLifecycleMessageDataEvent() {
+    }
+
+    public MissionLifecycleMessageDataEvent(String source, com.redhat.cajun.navy.rules.model.Mission body, String kogitoProcessinstanceId, String kogitoParentProcessinstanceId, String kogitoRootProcessinstanceId, String kogitoProcessId, String kogitoRootProcessId, String kogitoProcessinstanceState, String kogitoReferenceId) {
+        this("MissionLifecycleMessageDataEvent", source, body, kogitoProcessinstanceId, kogitoParentProcessinstanceId, kogitoRootProcessinstanceId, kogitoProcessId, kogitoRootProcessId, kogitoProcessinstanceState, kogitoReferenceId);
+    }
+
+    public MissionLifecycleMessageDataEvent(String type, String source, com.redhat.cajun.navy.rules.model.Mission body, String kogitoProcessinstanceId, String kogitoParentProcessinstanceId, String kogitoRootProcessinstanceId, String kogitoProcessId, String kogitoRootProcessId, String kogitoProcessinstanceState, String kogitoReferenceId) {
+        super(type, source, body, kogitoProcessinstanceId, kogitoParentProcessinstanceId, kogitoRootProcessinstanceId, kogitoProcessId, kogitoRootProcessId, kogitoProcessinstanceState, null, kogitoReferenceId);
+    }
+}
+
+class MissionWrapper {
+    private byte[] missionBytes;
+
+    public byte[] getMissionBytes() {
+        return missionBytes;
+    }
+
+    public void setMissionBytes(byte[] x) {
+        missionBytes = x;
     }
 }
